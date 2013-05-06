a909eff89ed8fc543a10883e47d98959b38b0ed1
The Mini John <admin@phoenix-prime.gr>
2013-05-06 12:04:14 +0300

    commit after fix

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = '83cbb6883e699a5ee8b2f2684972fc47fb4d533165c8f03c07f0bdcfa1e68a0564168514089599bd9c4948666d9c7eb2630d896e50b6f948c707ac1f6ebd39c3'
